755022dc477dc1ca021ae30e15a2ead9c76faef4
Matt Zimmerman <mzimmerman@gmail.com>
2015-11-02 14:47:47 -0500

    Adding in a timezone to parse data from

--- tt.go
+++ tt.go
@@ -11,7 +11,8 @@ import (
 var from = flag.String("from", "", "Time layout to convert from, specified in reference time - see https://golang.org/src/time/format.go#L61")
 var to = flag.String("to", "", "Time layout to convert to, specified in reference time - see https://golang.org/src/time/format.go#L61")
 var convertTime = flag.Bool("convert", false, "Convert all times to timezone (default UTC)")
-var timezone = flag.String("timezone", "UTC", "Timezone to convert date to with -convert")
+var totimezone = flag.String("totimezone", "UTC", "Timezone to convert date to with -convert")
+var fromtimezone = flag.String("fromtimezone", "UTC", "Timezone to parse date from (default UTC)")
 
 var helpURL = "https://golang.org/src/time/format.go#L64"
 
@@ -31,34 +32,35 @@ func main() {
 	lineReader := bufio.NewScanner(bufio.NewReader(os.Stdin))
 	lineWriter := bufio.NewWriter(os.Stdout)
 	defer lineWriter.Flush()
-	firstLine := true
-	var tz *time.Location
+	var totz *time.Location
+	var fromtz *time.Location
 	var err error
 	if *convertTime {
-		tz, err = time.LoadLocation(*timezone)
+		totz, err = time.LoadLocation(*totimezone)
 		if err != nil {
-			log.Fatalf("Error loading timezone - %s - %v", *timezone, err)
+			log.Fatalf("Error loading timezone - %s - %v", *totimezone, err)
 		}
+	}
+	fromtz, err = time.LoadLocation(*fromtimezone)
+	if err != nil {
+		log.Fatalf("Error loading timezone - %s - %v", *fromtimezone, err)
 	}
 	for {
 		if !lineReader.Scan() {
 			break
 		}
-		if !firstLine {
-			lineWriter.WriteString("\n")
-			firstLine = false
-		}
 		line := lineReader.Text()
-		date, err := time.Parse(*from, line)
+		date, err := time.ParseInLocation(*from, line, fromtz)
 		if err != nil {
 			log.Fatalf("Error parsing input time - %v", err)
 		}
 		if *convertTime {
-			date = date.In(tz)
+			date = date.In(totz)
 		}
-		lineWriter.WriteString(date.Format(*to))
+		_, err = lineWriter.WriteString(date.Format(*to))
 		if err != nil {
 			log.Fatalf("Error writing output time - %v", err)
 		}
+		lineWriter.WriteString("\n")
 	}
 }
